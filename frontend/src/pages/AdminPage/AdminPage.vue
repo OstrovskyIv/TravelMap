eb4be6e22e2658859b1fe2aafe266be369990fc4
OstrovskyIv <ostrovskyiml@gmail.com>
2026-01-17 23:53:49 +0300

    Изменил пару блоков в админ панели

--- frontend/src/pages/AdminPage/AdminPage.vue
+++ frontend/src/pages/AdminPage/AdminPage.vue
@@ -1,26 +1,15 @@
 <template>
   <div class="fixed inset-0 z-[60] flex bg-[#0c0c0e] text-white font-sans overflow-hidden">
-
-    <button
-        @click="router.push('/')"
-        class="absolute top-6 right-8 z-50 px-5 py-2 bg-white/5 hover:bg-white/10 border border-white/10 rounded-xl text-[10px] font-black uppercase tracking-widest transition-all active:scale-95 shadow-2xl backdrop-blur-xl"
-    >
+    <button @click="router.push('/')" class="absolute top-6 right-8 z-50 px-5 py-2 bg-white/5 hover:bg-white/10 border border-white/10 rounded-xl text-[10px] font-black uppercase tracking-widest transition-all active:scale-95 shadow-2xl backdrop-blur-xl">
       {{ langStore.currentLang === 'ru' ? 'Выйти из панели' : 'Exit Console' }}
     </button>
-
     <aside class="w-72 h-full border-r border-white/5 flex flex-col bg-white/[0.01]">
       <header class="p-8 pb-4 flex flex-col gap-1">
         <h2 class="text-lg font-black uppercase tracking-tighter italic text-blue-500">System</h2>
         <div class="h-[1px] w-8 bg-white/10"></div>
       </header>
-
       <nav class="flex-1 overflow-y-auto p-4 flex flex-col gap-1 custom-scrollbar">
-        <button
-            v-for="tab in tabs" :key="tab.id"
-            @click="activeTabId = tab.id"
-            class="w-full flex items-center gap-4 p-4 rounded-xl transition-all duration-200 group text-left"
-            :class="activeTabId === tab.id ? 'bg-white/5 shadow-inner' : 'hover:bg-white/[0.03]'"
-        >
+        <button v-for="tab in tabs" :key="tab.id" @click="activeTabId = tab.id" class="w-full flex items-center gap-4 p-4 rounded-xl transition-all duration-200 group text-left" :class="activeTabId === tab.id ? 'bg-white/5 shadow-inner' : 'hover:bg-white/[0.03]'">
           <span class="text-xl transition-transform group-hover:scale-110">{{ tab.icon }}</span>
           <span class="flex flex-col gap-0.5">
             <span class="text-[11px] font-bold uppercase tracking-wider" :class="activeTabId === tab.id ? 'text-white' : 'text-stone-500 group-hover:text-stone-300'">
