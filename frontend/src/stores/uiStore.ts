bcb9c2c5facd8618ae561510228cf10666043bf1
OstrovskyIv <ostrovskyiml@gmail.com>
2026-01-22 13:27:09 +0300

    изменил стор добавив логику окна

--- frontend/src/stores/uiStore.ts
+++ frontend/src/stores/uiStore.ts
@@ -3,10 +3,20 @@ import { ref } from 'vue'
 
 export const useUiStore = defineStore('ui', () => {
     const isSidebarOpen = ref(true)
+    const isAdminConsoleOpen = ref(false)
 
     const toggleSidebar = () => {
         isSidebarOpen.value = !isSidebarOpen.value
     }
 
-    return { isSidebarOpen, toggleSidebar }
+    const toggleAdminConsole = () => {
+        isAdminConsoleOpen.value = !isAdminConsoleOpen.value
+    }
+
+    return {
+        isSidebarOpen,
+        isAdminConsoleOpen,
+        toggleSidebar,
+        toggleAdminConsole
+    }
 })
