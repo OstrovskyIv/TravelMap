a0fdc6f5dbb2e74a0ca6d3aa83ab72077db1ce4c
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-11 14:32:06 +0300

    Заменил старые объекты

--- frontend/src/pages/ShopPage/ShopPage.vue
+++ frontend/src/pages/ShopPage/ShopPage.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="w-full h-full p-12 flex flex-col gap-16 overflow-y-auto custom-scrollbar font-sans transition-colors duration-1000" :style="{ backgroundColor: store.currentTheme?.background }">
 
-    <!-- ХЕДЕР И БАЛАНС -->
     <header class="flex justify-between items-start">
       <div class="flex flex-col gap-2">
         <h1 class="text-7xl font-black uppercase tracking-tighter italic text-white leading-none">
@@ -10,7 +9,6 @@
         <span class="text-xs font-mono uppercase tracking-[0.5em] opacity-40 text-white">Visual Engine Marketplace</span>
       </div>
 
-      <!-- Кошелек: Стиль как у счетчика на карте -->
       <div class="flex flex-col gap-2 items-end p-8 bg-[#18181b]/80 border border-[#fbbf24]/20 rounded-[40px] backdrop-blur-xl shadow-2xl">
         <span class="text-[10px] font-black uppercase tracking-[0.3em] text-[#fbbf24]">Available Stars</span>
         <div class="flex items-center gap-4">
@@ -20,7 +18,6 @@
       </div>
     </header>
 
-    <!-- СЕКЦИЯ 1: ТВОЯ КОЛЛЕКЦИЯ (КУПЛЕННЫЕ) -->
     <section class="flex flex-col gap-8">
       <div class="flex items-center gap-4 px-4">
         <h2 class="text-xs font-black uppercase tracking-[0.4em] text-white opacity-30">
@@ -55,7 +52,7 @@
       </div>
     </section>
 
-    <!-- СЕКЦИЯ 2: РЫНОК (НЕ КУПЛЕННЫЕ) -->
+
     <section v-if="availableThemes.length > 0" class="flex flex-col gap-8">
       <div class="flex items-center gap-4 px-4">
         <h2 class="text-xs font-black uppercase tracking-[0.4em] text-white opacity-30">
@@ -69,7 +66,6 @@
             v-for="theme in availableThemes" :key="theme.id"
             class="p-10 rounded-[48px] border border-white/5 bg-black/20 flex flex-col gap-8 relative overflow-hidden group shadow-xl"
         >
-          <!-- Эффект заблокированного контента -->
           <div class="flex justify-between items-center w-full relative z-10">
             <span class="w-16 h-16 rounded-3xl flex items-center justify-center text-3xl bg-white/[0.02] border border-white/5 grayscale">
                {{ theme.id === 'wooden' ? '🪵' : '✨' }}
@@ -82,7 +78,6 @@
             <span class="text-[9px] font-mono opacity-10 uppercase tracking-widest text-white">Requires purchase</span>
           </span>
 
-          <!-- Кнопка покупки -->
           <button
               @click="userStore.buyTheme(theme.id, theme.price)"
               class="w-full py-5 rounded-2xl flex items-center justify-center gap-3 transition-all active:scale-95 border border-transparent shadow-2xl relative z-10"
